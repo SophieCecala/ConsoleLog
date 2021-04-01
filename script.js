1aba0757b48e318c659c7a447f70de3c7c00e01c
Natalya Myunster <natalyamyunster@yahoo.com>
2021-04-01 13:27:42 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+count=7;
+time=30;
+name='Sophie';
+
+console.log(
+    "player: %s has %d point at %d seconds of the game.",
+    name, count, time);
+
+// task1
+
+a=10*2*2*2*2*2*2*2*2*2*2;
+
+console.log(a);
+
+// task2
+
+x= 32;
+y=-5*x+2;
+
+console.log("If x=1, then y:-3");
+
+// task3
+
+count=7;
+time=30;
+name='Nikita';
+
+console.log(
+    "player: %s at %d seconds of the game gained %d points",
+    name, time, count
+);
+
+// examples
+
+alert('Hello a wonderful world!');
+
+var str='Hello a wounderful world!';
+    alert(str);
+
